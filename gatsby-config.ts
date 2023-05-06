1233eec2a4a0e44fe8938a4184ca6d58141e5a73
Jeffry <jeffryklew@gmail.com>
2023-05-06 18:22:35 -0400

    Update siteUrl in gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `HockeyRATS`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `http://www.hockeyrats.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
