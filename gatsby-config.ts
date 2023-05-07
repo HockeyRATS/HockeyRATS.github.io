5cc4a54eb0b6ab5b55e43870e78dc027777bebc3
Jeffry <jeffryklew@gmail.com>
2023-05-07 12:29:40 -0400

    Run prettier on gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,20 +1,24 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `HockeyRATS`,
-    siteUrl: `http://www.hockeyrats.com`
+    siteUrl: `http://www.hockeyrats.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
-};
+  plugins: [
+    "gatsby-plugin-google-gtag",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+}
 
-export default config;
+export default config
