a26292c35f86266fad71b354467090f9aacbf484
Jeffry <jeffryklew@gmail.com>
2023-05-09 07:32:36 -0400

    Incorporate Layout into src/pages/lessons.tsx

--- src/pages/lessons.tsx
+++ src/pages/lessons.tsx
@@ -1,31 +1,34 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const LessonsPage = () => {
   return (
-    <main>
-      <h1>Lessons</h1>
+    <Layout>
+      <main>
+        <h1>Lessons</h1>
 
-      <p>
-        To experience the R.A.T.S. Hockey Training System, please view our
-        available lessons below.
-      </p>
+        <p>
+          To experience the R.A.T.S. Hockey Training System, please view our
+          available lessons below.
+        </p>
 
-      <p>
-        Lessons are held at{" "}
-        <a
-          href="https://ice-land.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Ice Land Rink
-        </a>{" "}
-        in Hamilton Township, NJ. Please <Link to="/contact">contact me</Link>{" "}
-        regarding availability or if lessons at other facilities are desired.
-      </p>
+        <p>
+          Lessons are held at{" "}
+          <a
+            href="https://ice-land.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Ice Land Rink
+          </a>{" "}
+          in Hamilton Township, NJ. Please <Link to="/contact">contact me</Link>{" "}
+          regarding availability or if lessons at other facilities are desired.
+        </p>
 
-      <h1>Sample Lessons</h1>
-    </main>
+        <h1>Sample Lessons</h1>
+      </main>
+    </Layout>
   )
 }
 
