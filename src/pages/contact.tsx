0d2d3d55cefbff1bed1847078753327bcec3b9e0
Jeffry <jeffryklew@gmail.com>
2023-05-09 07:32:53 -0400

    Incorporate Layout into src/pages/contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,30 +1,33 @@
 import * as React from "react"
+import Layout from "../components/layout"
 
 const ContactPage = () => {
   return (
-    <main>
-      <h1>Contact</h1>
+    <Layout>
+      <main>
+        <h1>Contact</h1>
 
-      <div>
-        <p>For inquiries, please reach me at:</p>
-        <ul>
-          <li>
-            Call/Text <a href="tel:+19088120636">(908) 812-0636</a>
-          </li>
-          <li>
-            <a href="mailto:joe@hockeyrats.com">joe@hockeyrats.com</a>
-          </li>
-        </ul>
+        <div>
+          <p>For inquiries, please reach me at:</p>
+          <ul>
+            <li>
+              Call/Text <a href="tel:+19088120636">(908) 812-0636</a>
+            </li>
+            <li>
+              <a href="mailto:joe@hockeyrats.com">joe@hockeyrats.com</a>
+            </li>
+          </ul>
 
-        <h3>Ice Land Rink</h3>
-        <ul>
-          <li>
-            Call <a href="tel:+16095886672">(609) 588-6672</a>
-          </li>
-          <li>6 Tennis Court, Hamilton Township, NJ 08619</li>
-        </ul>
-      </div>
-    </main>
+          <h3>Ice Land Rink</h3>
+          <ul>
+            <li>
+              Call <a href="tel:+16095886672">(609) 588-6672</a>
+            </li>
+            <li>6 Tennis Court, Hamilton Township, NJ 08619</li>
+          </ul>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
