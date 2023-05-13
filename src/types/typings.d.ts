99c6bfe011ebbc412ed70567da31fb44c0edc015
Jeffry <jeffryklew@gmail.com>
2023-05-13 18:49:04 -0400

    Add .png to src/types/typings.d.ts

--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -3,6 +3,10 @@ declare module "*.jpg" {
   const value: any
   export = value
 }
+declare module "*.png" {
+  const value: any
+  export = value
+}
 declare module "*.mp4" {
   const value: any
   export = value
