56aff0807efb23a54a217baa9ad8b18ec8b1b83b
Jeffry <jeffryklew@gmail.com>
2023-05-14 12:10:43 -0400

    Adjust centering and styling of iframes in lessons.tsx

--- src/pages/lessons.tsx
+++ src/pages/lessons.tsx
@@ -6,10 +6,10 @@ import Ratio from "react-bootstrap/Ratio"
 import Layout from "../components/layout"
 import {
   bodyStyle,
-  calendarStyle,
+  calendarRatioParentStyle,
   jumbotronStyle,
-  ratioParentStyle,
-  videoStyle,
+  jumbotronVideoStyle,
+  videoRatioParentStyle,
 } from "./lessons.module.css"
 
 const videoPath: string =
@@ -26,14 +26,14 @@ const LessonsPage: React.FC<PageProps> = () => {
           autoPlay
           loop
           poster="../images/HockeyRATS_lessons_jumbotron_poster.png"
-          className={videoStyle}
+          className={jumbotronVideoStyle}
         >
           <source src={videoPath} type="video/mp4" />
         </video>
       </Container>
 
       {/* Lessons Information */}
-      <Container className="mt-4">
+      <Container className="mt-5">
         <h1 className="display-4 text-center mb-5">Lessons</h1>
 
         <p className="pb-3">
@@ -55,68 +55,72 @@ const LessonsPage: React.FC<PageProps> = () => {
         </p>
 
         {/* Lessons Calendar from Google */}
-        <Container className="mb-4 d-flex justify-content-center d-md-none">
-          <Container className={ratioParentStyle}>
+        <Container className="d-flex justify-content-center d-md-none">
+          <Container className={calendarRatioParentStyle}>
             <Ratio aspectRatio="4x3">
               <iframe
-                className={calendarStyle}
+                className="border border-0"
                 src="https://calendar.google.com/calendar/embed?height=300&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showNav=1&showDate=1&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=WEEK&src=aG9ja2V5cmF0c29mZmljaWFsQGdtYWlsLmNvbQ&color=%23039BE5"
               ></iframe>
             </Ratio>
           </Container>
         </Container>
 
-        <Container className="mb-4 d-none d-md-flex justify-content-center d-lg-none">
-          <Container className={ratioParentStyle}>
+        <Container className="d-none d-md-flex justify-content-center d-lg-none">
+          <Container className={calendarRatioParentStyle}>
             <Ratio aspectRatio="4x3">
               <iframe
-                className={calendarStyle}
+                className="border border-0"
                 src="https://calendar.google.com/calendar/embed?height=450&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showNav=1&showDate=1&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=aG9ja2V5cmF0c29mZmljaWFsQGdtYWlsLmNvbQ&color=%23039BE5"
               ></iframe>
             </Ratio>
           </Container>
         </Container>
 
-        <Container className="mb-4 d-none d-lg-flex justify-content-center d-xxl-none">
-          <Container className={ratioParentStyle}>
+        <Container className="d-none d-lg-flex justify-content-center d-xxl-none">
+          <Container className={calendarRatioParentStyle}>
             <Ratio aspectRatio="4x3">
               <iframe
-                className={calendarStyle}
+                className="border border-0"
                 src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showNav=1&showDate=1&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=aG9ja2V5cmF0c29mZmljaWFsQGdtYWlsLmNvbQ&color=%23039BE5"
               ></iframe>
             </Ratio>
           </Container>
         </Container>
 
-        <Container className="mb-4 d-none d-xxl-flex justify-content-center">
-          <Container className={ratioParentStyle}>
-            <Ratio aspectRatio="4x3">
+        <Container className="d-none d-xxl-flex justify-content-center">
+          <Container className={calendarRatioParentStyle}>
+            <Ratio aspectRatio="16x9">
               <iframe
-                className={calendarStyle}
+                className="border border-0"
                 src="https://calendar.google.com/calendar/embed?height=900&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showNav=1&showDate=1&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=aG9ja2V5cmF0c29mZmljaWFsQGdtYWlsLmNvbQ&color=%23039BE5"
               ></iframe>
             </Ratio>
           </Container>
         </Container>
 
-        <h1 className="display-5 text-center my-5">Sample Lessons</h1>
+        <h1 className="display-5 text-center mt-4 mb-5">Sample Lessons</h1>
 
-        <Container className="d-flex justify-content-center mb-5">
-          <Ratio aspectRatio="16x9">
-            <iframe
-              src="https://www.youtube-nocookie.com/embed/p78VOFA6_CU"
-              allowFullScreen
-            ></iframe>
-          </Ratio>
+        <Container className="d-flex justify-content-center mb-4">
+          <Container className={videoRatioParentStyle}>
+            <Ratio aspectRatio="16x9">
+              <iframe
+                src="https://www.youtube-nocookie.com/embed/p78VOFA6_CU"
+                allowFullScreen
+              ></iframe>
+            </Ratio>
+          </Container>
         </Container>
 
         <Container className="d-flex justify-content-center">
-          <Ratio aspectRatio="16x9">
-            <iframe
-              src="https://www.youtube-nocookie.com/embed/l5PLL66Pw9s"
-              allowFullScreen
-            ></iframe>
-          </Ratio>
+          <Container className={videoRatioParentStyle}>
+            <Ratio aspectRatio="16x9">
+              <iframe
+                src="https://www.youtube-nocookie.com/embed/l5PLL66Pw9s"
+                allowFullScreen
+              ></iframe>
+            </Ratio>
+          </Container>
         </Container>
       </Container>
     </Layout>
